3b432a2bc3e1e4edc3e19bdb5af54246ec9cd991
nev <cy823236858@gmail.com>
2024-02-03 21:01:20 +0800

    initial debugging

---
+++ scripts_zero123/run_blender.py
@@ -0,0 +1,116 @@
+import glob
+import json
+import multiprocessing
+import shutil
+import subprocess
+import time
+from dataclasses import dataclass
+from typing import Optional
+import os
+
+import boto3
+import tyro
+import wandb
+import math
+
+
+@dataclass
+class Args:
+    workers_per_gpu: int
+    """number of workers per gpu"""
+
+    input_models_path: str
+    """Path to a json file containing a list of 3D object files"""
+
+    upload_to_s3: bool = False
+    """Whether to upload the rendered images to S3"""
+
+    log_to_wandb: bool = False
+    """Whether to log the progress to wandb"""
+
+    num_gpus: int = -1
+    """number of gpus to use. -1 means all available gpus"""
+
+
+def worker(
+    queue: multiprocessing.JoinableQueue,
+    count: multiprocessing.Value,
+    gpu: int,
+    s3: Optional[boto3.client],
+) -> None:
+    while True:
+        item = queue.get()
+        if item is None:
+            break
+
+        view_path = os.path.join('.objaverse/hf-objaverse-v1/views_whole_sphere', item.split('/')[-1][:-4])
+        if os.path.exists(view_path):
+            queue.task_done()
+            print('========', item, 'rendered', '========')
+            continue
+        else:
+            os.makedirs(view_path, exist_ok = True)
+
+        # Perform some operation on the item
+        print(item, gpu)
+        command = (
+            # f"export DISPLAY=:0.{gpu} &&"
+            # f" GOMP_CPU_AFFINITY='0-47' OMP_NUM_THREADS=48 OMP_SCHEDULE=STATIC OMP_PROC_BIND=CLOSE "
+            f" CUDA_VISIBLE_DEVICES={gpu} "
+            f" blender-3.2.2-linux-x64/blender -b -P scripts/blender_script.py --"
+            f" --object_path {item}"
+        )
+        print(command)
+        subprocess.run(command, shell=True)
+
+        with count.get_lock():
+            count.value += 1
+
+        queue.task_done()
+
+
+if __name__ == "__main__":
+    args = tyro.cli(Args)
+
+    s3 = boto3.client("s3") if args.upload_to_s3 else None
+    queue = multiprocessing.JoinableQueue()
+    count = multiprocessing.Value("i", 0)
+
+    import subprocess
+
+    # cmd = ['/run/myscript', '--arg', 'value']
+    # result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    # print(result.stdout)
+
+    # if args.log_to_wandb:
+    #     wandb.init(project="objaverse-rendering", entity="prior-ai2")
+    #
+    # # Start worker processes on each of the GPUs
+    # for gpu_i in range(args.num_gpus):
+    #     for worker_i in range(args.workers_per_gpu):
+    #         worker_i = gpu_i * args.workers_per_gpu + worker_i
+    #         process = multiprocessing.Process(
+    #             target=worker, args=(queue, count, gpu_i, s3)
+    #         )
+    #         process.daemon = True
+    #         process.start()
+
+    # Add items to the queue
+    with open(args.input_models_path, "r") as f:
+        model_paths = json.load(f)
+
+    c = 0
+    for item in model_paths:
+        print("item_id ,", c )
+        c+=1
+
+        cmd = ['blender-3.2.2-linux-x64/blender  -b -P scripts/blender_script.py -- ', '--object_path', item , '--output_dir', './views', '--engine' 'CYCLES', '--num_images','12' ]
+        result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+        print(result.stdout)
+
+    # # Wait for all tasks to be completed
+    # queue.join()
+    #
+    # # Add sentinels to the queue to stop the worker processes
+    # for i in range(args.num_gpus * args.workers_per_gpu):
+    #     queue.put(None)
