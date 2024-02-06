bcfbade80832a7e6c0488950eb907c220401af58
nev <cy823236858@gmail.com>
2024-02-06 11:04:19 +0800

    initial debugging

--- check_len.py
+++ check_len.py
@@ -5,7 +5,7 @@ import json
 x = os.listdir('views_valid5')
 print(len(x))
 
-with open('valid5.json', "r") as f:
+with open('valid_paths_5.json', "r") as f:
     model_paths = json.load(f)
 
 print("json len ", len(model_paths))
